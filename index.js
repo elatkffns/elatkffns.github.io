83a64e4c47a4c219ce7fd39a66aca0d4876f83d8
sh <elatkffns@naver.com>
2023-02-15 16:54:08 +0900

    layout

---
+++ index.js
@@ -0,0 +1,6 @@
+let btn = document.getElementsByClassName("btn")[0];
+btn.addEventListener("click", () => {
+  btn.style.display = "none";
+  // let hide = document.getElementsByClassName("hide")[0];
+  // hide.style.display = "none";
+});
