14fa3d88a1ff6d3978cdf9a935d896bddac4070b
elatkffns <116622266+elatkffns@users.noreply.github.com>
2023-03-03 09:23:54 +0900

    ec2, mariadb dump

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let btn = document.getElementsByClassName("btn")[0];
-btn.addEventListener("click", () => {
-  btn.style.display = "none";
-  // let hide = document.getElementsByClassName("hide")[0];
-  // hide.style.display = "none";
+const mysql = require("mariadb");
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "comicbook",
+  port: "3306",
 });
+conn.connect();
+
+conn.query(
+  "create table books (number int not null auto_increment primary key, genre varchar(20) not null, name varchar(50) not null, writer varchar(30) not null, releasedate date not null);",
+  (error, result, fields) => {
+    if (error) throw error;
+    console.log(result);
+  }
+);
+conn.query("describe books", (error, result, fields) => {
+  if (error) throw error;
+  console.log(result);
+});
+conn.end();
